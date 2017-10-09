e9103c630df2e90c82cbe9bf6eac59b4c27b09c7
Bart <b.smeets1@gmail.com>
2017-10-09 15:58:54 +0200

    Demander class toegevoegd

---
+++ code/src/main/java/li/bart/zwerverapp/Demander.java
@@ -0,0 +1,60 @@
+package li.bart.zwerverapp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Demander {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private String photoURL;
+	private String description;
+	private int numberOfRequests;
+	private String locationGPS;
+	
+	public Demander() {
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhotoURL() {
+		return photoURL;
+	}
+
+	public void setPhotoURL(String photoURL) {
+		this.photoURL = photoURL;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getNumberOfRequests() {
+		return numberOfRequests;
+	}
+
+	public void setNumberOfRequests(int numberOfRequests) {
+		this.numberOfRequests = numberOfRequests;
+	}
+
+	public String getLocationGPS() {
+		return locationGPS;
+	}
+
+	public void setLocationGPS(String locationGPS) {
+		this.locationGPS = locationGPS;
+	}
+
+}
