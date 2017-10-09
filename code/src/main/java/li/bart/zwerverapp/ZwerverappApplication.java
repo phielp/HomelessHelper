06690e02c8e9ba6320eb636a907ba4c2f3f03461
Bart <b.smeets1@gmail.com>
2017-10-09 15:16:27 +0200

    werwer
    Het eerste begin

---
+++ code/src/main/java/li/bart/zwerverapp/ZwerverappApplication.java
@@ -0,0 +1,12 @@
+package li.bart.zwerverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwerverappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwerverappApplication.class, args);
+	}
+}
---
+++ code/src/test/java/li/bart/zwerverapp/ZwerverappApplicationTests.java
@@ -0,0 +1,16 @@
+package li.bart.zwerverapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZwerverappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
