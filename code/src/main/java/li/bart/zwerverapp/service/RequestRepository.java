fe1793f3ce410721f854de850171782ecd3fff35
phielp <phielp@gmail.com>
2017-10-11 11:49:54 +0200

    The return of User.java

--- code/src/main/java/li/bart/zwerverapp/service/RequestRepository.java
+++ code/src/main/java/li/bart/zwerverapp/service/RequestRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 import li.bart.zwerverapp.User;
 
 @Component
-public interface HHRepository extends CrudRepository<User, Long> {
+public interface RequestRepository extends CrudRepository<Request, Long> {
 
 }
