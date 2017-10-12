a79c0a01d28e110f8b32943b62d9328b1a7bb214
Bart <b.smeets1@gmail.com>
2017-10-12 14:11:14 +0200

    delete werkend int abel

--- code/src/main/java/li/bart/zwerverapp/rest/HHEndpoint.java
+++ code/src/main/java/li/bart/zwerverapp/rest/HHEndpoint.java
@@ -3,6 +3,7 @@ package li.bart.zwerverapp.rest;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,7 +48,7 @@ public class HHEndpoint {
 		return hhservice.requestList();
 	}
 	
-	@GetMapping("/demander/{foo}")
+	@DeleteMapping("/demander/{foo}")
 	public void yoyo(@PathVariable long foo) {
 		hhservice.deleteRequest(foo);
 	}
