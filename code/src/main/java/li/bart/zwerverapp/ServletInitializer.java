e01f48f7713215d647baaf9249023b1f97251d18
Bart <b.smeets1@gmail.com>
2017-10-24 12:08:48 +0200

    aanpassingen

---
+++ code/src/main/java/li/bart/zwerverapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package li.bart.zwerverapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ZwerverappApplication.class);
+	}
+
+}
